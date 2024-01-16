2dbdb81b9713180c54a71810dcbf5533b394fde2
jormal <jormal@naver.com>
2024-01-16 14:35:48 +0900

    ASSET-9 Refactor image preprocess
    Signed-off-by: jormal <jormal@naver.com>

---
+++ app.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+from asyncio import run
+
+from libraries.preprocess import run_preprocess
+
+OPERATION_DICT = {"preprocess": run_preprocess}
+"""The dictionary of operations application supports."""
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Run application for asset-info-v2.")
+    parser.add_argument(
+        "operation",
+        type=str,
+        choices=OPERATION_DICT.keys(),
+        help="The operation to run.",
+    )
+    args = parser.parse_args()
+    run(OPERATION_DICT[args.operation]())
