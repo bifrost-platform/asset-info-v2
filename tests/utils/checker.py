2029422ea27fd78c517d99392d0b62290688e4e9
jormal <jormal@naver.com>
2024-01-16 15:55:36 +0900

    ASSET-9 Refine image validation
    Signed-off-by: jormal <jormal@naver.com>

--- tests/utils/checker.py
+++ tests/utils/checker.py
@@ -1,6 +1,11 @@
 import os
+from pathlib import Path
+
+from PIL import Image
+from svgpathtools import svg2paths2
 
 from libraries.models.image_info import ImageInfo
+from libraries.models.image_type import ImageTypeEnum
 from libraries.utils.file import File, PWD
 
 
@@ -18,69 +23,51 @@ def check_info_json_existence(model_dir_name: str) -> None:
         assert os.path.exists(sub_dir.joinpath("info.json"))
 
 
-def check_png128(image_info: ImageInfo, file: File) -> None:
-    """Check if 128x128 png image exists if `png128` of :class:`ImageInfo` is `True`.
+def __check_image_existence(image_path: Path, existence: bool) -> None:
+    """Check if image exists.
+
+    Args:
+        image_path: Path of the image.
+        existence: File object of the image.
+    """
+    if existence:
+        assert os.path.isfile(image_path)
+    else:
+        assert not os.path.isfile(image_path)
+
+
+def __check_image_size(image_type: ImageTypeEnum, image_path: Path) -> None:
+    """Check if image size is correct.
+
+    Args:
+        image_type: Type of the image.
+        image_path: Path of the image.
+    """
+    if image_type.is_png():
+        with Image.open(image_path) as img:
+            assert img.size[0] == image_type.get_size()
+            assert img.size[1] == image_type.get_size()
+    elif image_type.is_svg():
+        _, _, attributes = svg2paths2(image_path)
+        assert int(attributes["width"]) == image_type.get_size()
+        assert int(attributes["height"]) == image_type.get_size()
+    else:
+        raise AssertionError(f"Unknown image type: {image_type}")
+
+
+def __check_image_preprocessed(image_info: ImageInfo) -> None:
+    """Check if all images are preprocessed.
 
     Args:
         image_info: Information of image.
-        file: File object of the image.
     """
-    if image_info.png128:
-        assert os.path.isfile(file.path.parent.joinpath("image-128.png"))
-    else:
-        assert not os.path.isfile(file.path.parent.joinpath("image-128.png"))
-
-
-def check_png256(image_info: ImageInfo, file: File) -> None:
-    """Check if 256x256 png image exists if `png256` of :class:`ImageInfo` is `True`.
-
-    Args:
-        image_info: Information of image.
-        file: File object of the image.
-    """
-    if image_info.png256:
-        assert os.path.isfile(file.path.parent.joinpath("image-256.png"))
-    else:
-        assert not os.path.isfile(file.path.parent.joinpath("image-256.png"))
-
-
-def check_png32(image_info: ImageInfo, file: File) -> None:
-    """Check if 32x32 png image exists if `png32` of :class:`ImageInfo` is `True`.
-
-    Args:
-        image_info: Information of image.
-        file: File object of the image.
-    """
-    if image_info.png32:
-        assert os.path.isfile(file.path.parent.joinpath("image-32.png"))
-    else:
-        assert not os.path.isfile(file.path.parent.joinpath("image-32.png"))
-
-
-def check_png64(image_info: ImageInfo, file: File) -> None:
-    """Check if 64x64 png image exists if `png64` of :class:`ImageInfo` is `True`.
-
-    Args:
-        image_info: Information of image.
-        file: File object of the image.
-    """
-    if image_info.png64:
-        assert os.path.isfile(file.path.parent.joinpath("image-64.png"))
-    else:
-        assert not os.path.isfile(file.path.parent.joinpath("image-64.png"))
-
-
-def check_svg(image_info: ImageInfo, file: File) -> None:
-    """Check if svg image exists if `svg` of :class:`ImageInfo` is `True`.
-
-    Args:
-        image_info: Information of image.
-        file: File object of the image.
-    """
-    if image_info.svg:
-        assert os.path.isfile(file.path.parent.joinpath("image.svg"))
-    else:
-        assert not os.path.isfile(file.path.parent.joinpath("image.svg"))
+    exist_images = [
+        idx
+        for idx, image_type in enumerate(ImageTypeEnum.get_ascending_type_list())
+        if image_info.model_dump()[image_type.value]
+    ]
+    max_image = 0 if len(exist_images) == 0 else max(exist_images) + 1
+    assert len(exist_images) == max_image
 
 
 def check_images_validity(image_info: ImageInfo, file: File) -> None:
@@ -90,8 +77,10 @@ def check_images_validity(image_info: ImageInfo, file: File) -> None:
         image_info: Information of image.
         file: File object of the image.
     """
-    check_png128(image_info, file)
-    check_png256(image_info, file)
-    check_png32(image_info, file)
-    check_png64(image_info, file)
-    check_svg(image_info, file)
+    __check_image_preprocessed(image_info)
+    for image, existence in image_info.model_dump().items():
+        image_type = ImageTypeEnum(image)
+        image_path = image_type.get_path(file.path.parent)
+        __check_image_existence(image_path, existence)
+        if existence:
+            __check_image_size(image_type, image_path)
