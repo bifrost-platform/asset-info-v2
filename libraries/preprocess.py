a5b4f034ac2dfe9de597d28fb04f3d7127bae9a7
jormal <jormal@naver.com>
2024-01-18 16:22:19 +0900

    ASSET-5 Add enum ID preprocess
    Signed-off-by: jormal <jormal@naver.com>

--- libraries/preprocess.py
+++ libraries/preprocess.py
@@ -1,3 +1,7 @@
+from libraries.models.asset import Asset
+from libraries.models.network import Network
+from libraries.models.protocol import Protocol
+from libraries.utils.enum_info import update_id_enum
 from libraries.utils.file import PWD
 from libraries.utils.image import get_base_image_list, create_downscaled_image
 
@@ -13,6 +17,13 @@ def __run_image_preprocess() -> None:
         create_downscaled_image(image_path.path)
 
 
+def __run_enum_preprocess() -> None:
+    """Update enum information from the information of asset, network and protocol."""
+    for model_type in [Asset, Network, Protocol]:
+        update_id_enum(model_type)
+
+
 def run_preprocess() -> None:
     """Run preprocess list."""
     __run_image_preprocess()
+    __run_enum_preprocess()
