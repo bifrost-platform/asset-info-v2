0af80102aefc35a117589100699990ffa66c3a83
jormal <jormal@naver.com>
2024-05-31 18:58:06 +0900

    ASSET-26 Fix address type
    Signed-off-by: jormal <jormal@naver.com>

--- libraries/models/address.py
+++ libraries/models/address.py
@@ -1,31 +1,46 @@
-from typing import Annotated, Type, Union
+from typing import Union, Self
 
-from pydantic import StringConstraints, RootModel, AfterValidator
-from web3 import Web3
+from pydantic import RootModel
 
-EVM_ADDRESS_PATTERN: str = r"^0x[a-fA-F0-9]{40}$"
-"""Regex pattern for an Ethereum Virtual Machine (EVM) address."""
-
-EvmAddress: Type = RootModel[
-    Annotated[
-        str,
-        StringConstraints(pattern=EVM_ADDRESS_PATTERN),
-        AfterValidator(lambda x: Web3.to_checksum_address(x)),
-    ]
-]
-"""A constrained `str` for the EVM address."""
-
-Address: Type = RootModel[Union[EvmAddress]]
-"""A union of constrained `str` about each address of blockchain networks."""
+from libraries.models.address_evm import AddressEvm
 
 
-def is_evm_address(address: Address) -> bool:
-    """Check if the address is an EVM address.
+class Address(RootModel[Union[AddressEvm]]):
+    """A union of constrained `str` about each address of blockchain networks."""
 
-    Args:
-        address: The address to check.
+    def __eq__(self, other: Self) -> bool:
+        if self.is_evm_address and other.is_evm_address:
+            return AddressEvm.__eq__(self.root, other.root)
+        else:
+            raise ValueError(
+                f"Cannot compare difference addresses {type(self.root)} and {type(other.root)}."
+            )
 
-    Returns:
-        Whether the address is an EVM address.
-    """
-    return isinstance(address.root, EvmAddress)
+    def __ne__(self, other: Self) -> bool:
+        return not self.__eq__(other)
+
+    def __lt__(self, other: Self) -> bool:
+        if self.is_evm_address and other.is_evm_address:
+            return AddressEvm.__lt__(self.root, other.root)
+        else:
+            raise ValueError(
+                f"Cannot compare difference addresses {type(self.root)} and {type(other.root)}."
+            )
+
+    def __le__(self, other: Self) -> bool:
+        return self.__lt__(other) or self.__eq__(other)
+
+    def __gt__(self, other: Self) -> bool:
+        return not self.__le__(other)
+
+    def __ge__(self, other: Self) -> bool:
+        return not self.__lt__(other)
+
+    @property
+    def is_evm_address(self) -> bool:
+        """Check if the address is an EVM address.
+
+        Returns:
+            Whether the address is an EVM address.
+        """
+        return isinstance(self.root, AddressEvm)
